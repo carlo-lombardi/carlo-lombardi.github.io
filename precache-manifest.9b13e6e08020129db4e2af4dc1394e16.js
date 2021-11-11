199bca18cfe554d48db32acf486a1a427fb9b3b1
Alvaro P <alpattycruz@gmail.com>
2021-11-11 16:35:41 +0000

    Build added

---
+++ precache-manifest.9b13e6e08020129db4e2af4dc1394e16.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb06d0ddafaa2a4a06a93bcfde2f615c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad349fff0a6e31eaf9de",
+    "url": "/static/css/2.80b5d76c.chunk.css"
+  },
+  {
+    "revision": "1276cec83641023e534b",
+    "url": "/static/css/main.54beefdd.chunk.css"
+  },
+  {
+    "revision": "ad349fff0a6e31eaf9de",
+    "url": "/static/js/2.ed9fc787.chunk.js"
+  },
+  {
+    "revision": "1276cec83641023e534b",
+    "url": "/static/js/main.89c77693.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
